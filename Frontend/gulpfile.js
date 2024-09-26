5b54b64e853662d86e6fb0b96cabccad6b434d09
ayounsni <Ayoubsnini@gmail.com>
2024-09-26 17:25:00 +0100

    addComponent

--- Frontend/gulpfile.js
+++ Frontend/gulpfile.js
@@ -2,13 +2,13 @@ const { src , dest , watch , series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('Sass/index.scss')
+    return src('Sass/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['Sass/index.scss'], buildStyles)
+    watch(['Sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
